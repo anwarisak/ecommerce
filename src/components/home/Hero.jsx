d2a79f9ead88f767c6bc244831f0094ee0d14b79
anwarisak <anwarisack@gmail.com>
2026-02-17 23:32:37 +0300

    fix bug

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -4,45 +4,47 @@ import { Star, Heart, ChessQueen } from "lucide-react";
 
 const Hero = () => {
   return (
-    <section className="w-full max-w-5xl mx-auto overflow-x-hidden">
-      <div className="md:flex items-center justify-center bg-white p-8">
-        <div>
-          <h1 className="text-5xl max-w-md font-bold uppercase">
-            Use Fashion Into Your All Collection
+    <section className="w-full max-w-5xl mx-auto items-center overflow-x-hidden">
+      <div className="md:flex items-center justify-center bg-white-100 p-8 ">
+        <div className="">
+          <h1 className="text-5xl w-100 font-bold uppercase ">
+            Use Fashion Into Your All Colection
           </h1>
-
           <h4 className="text-2xl font-thin mt-8">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates,
+            asperiores!
           </h4>
-
-          <button className="h-14 px-8 mt-8 bg-black uppercase text-white rounded-full">
+          <button
+            className="h-14 w-50 mt-8 bg-black uppercase text-white rounded-full cursor-pointer"
+            type="button"
+          >
             Shop now
           </button>
         </div>
-
         <div className="mx-4">
           <img
-            className="max-w-full h-auto object-cover"
+            className="w-600 h-100 object-cover"
             src={heroimage}
             alt="hero"
           />
         </div>
 
-        <div>
-          <div className="flex items-center w-full md:w-48 h-16 rounded-lg shadow-lg my-8">
+        <div className="">
+          <div className="flex justify-center items-center w-full h-18 md:w-50 rounded-lg shadow-lg gap-0 my-8">
             <Star />
             <p className="text-2xl mx-4 font-light uppercase">Favourite</p>
           </div>
-
-          <div className="flex items-center w-full md:w-48 h-16 rounded-lg shadow-lg my-8">
+          <div className="flex justify-center items-center w-full h-18 md:w-50  rounded-lg shadow-lg gap-0 my-8">
             <Heart />
             <p className="text-2xl mx-4 font-light uppercase">Popular</p>
           </div>
 
-          <div className="flex flex-col items-center text-center w-full md:w-48 h-24 rounded-lg shadow-lg">
+          <div className="flex flex-col text-center justify-center items-center w-full h-25 md:w-50 md:h-28 rounded-lg shadow-lg">
             <ChessQueen />
-            <h5 className="text-lg my-2 font-light uppercase">Lifestyle</h5>
-            <p className="text-sm">Our brand follows the process</p>
+            <h5 className="text-lg mx-4 my-2 font-light uppercase">
+              LifeStyle
+            </h5>
+            <p className="text-sm mx-4">our brand follow the proccess</p>
           </div>
         </div>
       </div>
